0a3d65df78786ae389c91c3bde4f30701b069e5b
JesusAlvarez <jjalvarezgar@gmail.com>
2019-07-26 23:58:47 +0200

    creo un servicio
    creo un servicio llamado prueba

---
+++ src/app/servicios/prueba.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PruebaService {
+
+  constructor() {
+console.log('servicio listo para ser insertado');
+
+  }
+}
